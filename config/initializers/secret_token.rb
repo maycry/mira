e08924045314388725c20ee052fdb02717900951
maycry <byorickq@gmail.com>
2011-09-15 09:14:17 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mira::Application.config.secret_token = '100e294185773c4e6d7f827a33fb5dfed2e260ebeb294955323e40211a7d8481697377e5f97279e2b79823f5341e0511c3f9ea8d193eaf6b2ece6f405b11de1e'
