285afb0f95cacd6052e3168a19cd7b408e3b0a74
maycry <byorickq@gmail.com>
2011-09-15 21:32:58 +0300

    Scaffold lists and do some styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Mira::Application.routes.draw do
+  resources :lists
+
+  get "home/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +52,7 @@ Mira::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
