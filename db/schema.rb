3af119fabcb67fd5d40c08bec350beede59d3ada
maycry <byorickq@gmail.com>
2011-09-16 16:50:01 +0300

    Add user_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110915204806) do
+ActiveRecord::Schema.define(:version => 20110916134755) do
 
   create_table "items", :force => true do |t|
     t.string   "content"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(:version => 20110915204806) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
 end
