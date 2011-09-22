e52dd7a1eb930b0d9e14f92ac0d7d610b8f8b41c
maycry <byorickq@gmail.com>
2011-09-22 19:57:27 +0300

    in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
   def not_authenticated
     redirect_to login_url, :alert => "First login to access this page."
   end
